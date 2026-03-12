fce6c39b79b5b5ed809f3da108190941e30c428c
nefesercan-sys <nefesercan@gmail.com>
2026-03-12 16:12:55 +0300

    Update AuthProvider.tsx

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -1,5 +1,13 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
+
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      refetchInterval={5 * 60}
+      refetchOnWindowFocus={false}
+    >
+      {children}
+    </SessionProvider>
+  );
 }
