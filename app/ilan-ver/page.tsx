558c532ec87bef72b932bd75d05f9c444f199173
nefesercan-sys <nefesercan@gmail.com>
2026-03-16 20:32:50 +0300

    Update page.tsx

--- app/ilan-ver/page.tsx
+++ app/ilan-ver/page.tsx
@@ -12,7 +12,7 @@ import { COUNTRIES, getCitiesForCountry } from "@/lib/countries";
 import MedyaYukleyici from "@/app/components/MedyaYukleyici";
 
 function IlanVerIcerik() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
 
