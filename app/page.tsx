f6f24386a8e94458273f7e691d850e2bc91986ef
nefesercan-sys <nefesercan@gmail.com>
2026-03-17 03:06:36 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,6 @@ export default function AnaSayfa() {
       />
 
       <style dangerouslySetInnerHTML={{ __html: `
-        @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Unbounded:wght@700;800;900&display=swap');
 
         :root {
           --navy: #0d1b3e;
