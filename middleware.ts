9c3a200e111859762a2a07a89d8dc027169336b2
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 19:53:38 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  // Desteklenen tüm diller
-  locales: ['en', 'tr', 'de'],
-
-  // Eğer bir dil belirtilmemişse kullanılacak varsayılan dil
-  defaultLocale: 'tr',
-  
-  // URL'de varsayılan dilin görünüp görünmeyeceğini belirler (isteğe bağlı)
-  localePrefix: 'always' 
+  // Tüm dilleri buraya da ekle
+  locales: ['ar', 'de', 'en', 'es', 'fr', 'hi', 'ms', 'ru', 'tr', 'zh'],
+  defaultLocale: 'tr'
 });
 
 export const config = {
-  // Middleware'in hangi sayfalarda çalışacağını belirler. 
-  // Statik dosyaları (resimler, favicon vb.) hariç tutmak çok önemlidir.
-  matcher: [
-    // Tüm sayfaları yakala
-    '/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|images|icons).*)'
-  ]
+  matcher: ['/', '/(tr|en|de|ar|es|fr|hi|ms|ru|zh)/:path*']
 };
