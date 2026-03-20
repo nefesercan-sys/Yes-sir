a0caeb31d2ff477dcba7e7a804fb7b6e4bde9753
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 04:41:20 +0300

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,7 +2,6 @@ import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => {
   return {
-    // @/.. gibi işaretler yerine doğrudan üst klasöre çıkıyoruz
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
