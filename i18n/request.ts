ffbb0730fbce7939331c3d3afb99e59a0e166bb3
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 13:18:27 +0300

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -8,6 +8,6 @@ export default getRequestConfig(async ({ locale }) => {
 
   return {
     locale: locale as string,
-    messages: (await import(`@/messages/${locale}.json`)).default
+    messages: (await import(`../app/messages/${locale}.json`)).default
   };
 });
