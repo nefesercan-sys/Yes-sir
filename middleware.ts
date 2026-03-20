3c5dfec453714ab4294a86b40526844982ffbc75
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 10:24:58 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,7 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["tr", "en", "ar", "de", "ru", "zh", "es", "fr", "hi", "ms"],
   defaultLocale: "tr",
-  localePrefix: "always", // ← her zaman prefix kullan
-  localeDetection: false,
+  localePrefix: "as-needed",
 });
 
 export const config = {
