87dfe91cb225f1c02cfa053fb061eabc9a2c8f05
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 03:35:13 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ export default createMiddleware({
   locales: ["tr", "en", "ar", "de", "ru", "zh", "es", "fr", "hi", "ms"],
   defaultLocale: "tr",
   localePrefix: "as-needed",
-  localeDetection: false, // ← tarayıcı diline göre yönlendirme yapma
 });
 
 export const config = {
