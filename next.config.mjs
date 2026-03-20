6f940c84088adf3d325955a958f1188dc3e17ca1
nefesercan-sys <nefesercan@gmail.com>
2026-03-20 04:18:26 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Dosya yolunu parametre olarak vermiyoruz, default olarak src/i18n/request.ts'i arar
+// Parametre vermiyoruz, sistem src/i18n/request.ts dosyasını kendisi bulacak
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  // Hızlandırmak için build kontrollerini geçici olarak kapatıyoruz
+  // Derleme hızını artırmak ve ufak hataları görmezden gelmek için:
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
