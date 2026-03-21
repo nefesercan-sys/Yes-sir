5edba8ecf8b9decb62bc2b5490ae2abdde220a87
nefesercan-sys <nefesercan@gmail.com>
2026-03-21 11:42:11 +0300

    Delete i18n/request.ts

--- i18n/request.ts
+++
@@ -1,7 +0,0 @@
-// i18n/request.ts
-export default getRequestConfig(async ({ locale }) => {
-  return {
-    // Mesajlar app/messages altındaysa yol tam olarak budur:
-    messages: (await import(`../app/messages/${locale}.json`)).default
-  };
-});
