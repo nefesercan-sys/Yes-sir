c23757dfb91ca821d913b21acb65cbb57cfafaa6
nefesercan-sys <nefesercan@gmail.com>
2026-03-22 22:45:07 +0300

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+import { DefaultSession } from "next-auth";
