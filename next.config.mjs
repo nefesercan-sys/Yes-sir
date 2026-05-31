6e73ed3be1185d3be1ddbaac742f5aeafd5d25d1
nefesercan-sys <nefesercan@gmail.com>
2026-05-31 19:12:11 +0300

    Update next.config.mjs for image handling and redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,42 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+    unoptimized: true,
+  },
   async redirects() {
     return [
       {
-        source: '/ilan/:uuid([0-9a-f]{24})',
-        destination: '/ilan',
+        source: "/files/uuid{:b-f0-9]{s}",
+        destination: "/files",
         permanent: true,
       },
-    ]
+    ];
   },
-
   async headers() {
     return [
       {
-        source: '/(sitemap.*)\\.xml',
+        source: "/(sitemap-*).xml",
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400, stale-while-revalidate=43200',
+            key: "Cache-Control",
+            value: "public, max-age=86400, stale-while-revalidate=43200",
           },
         ],
+        permanent: true,
       },
-    ]
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
