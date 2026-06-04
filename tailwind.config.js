993f25ff197d144326096c1052fcfb4f356d03ac
nefesercan-sys <nefesercan@gmail.com>
2026-06-04 21:10:26 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false, // BU SATIR ÇOK ÖNEMLİ: Tailwind'in senin stillerini ezmesini engeller.
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
