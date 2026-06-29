23dc55764d9f5d3a3dfebe221386baa7ef622562
nefesercan-sys <nefesercan@gmail.com>
2026-06-30 00:23:40 +0300

    Update content paths in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-    "./providers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
